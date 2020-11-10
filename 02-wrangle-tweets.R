29c46753eb1746d14a8513b6ca4a4d2d65748670
Carlisle Rainey <carlislerainey@gmail.com>
2020-11-10 09:53:20 -0500

    Remove vote share from data

--- 02-wrangle-tweets.R
+++ 02-wrangle-tweets.R
@@ -17,26 +17,15 @@ r <- raw_tw %>%
   pivot_longer(cols = C:D) %>%
   select(-name) %>%
   separate(value, sep = ":|\\(|\\)", into = LETTERS, remove = FALSE, fill = "right") %>%
-  select(text, time, state, candidate = A, party = B, percent = D, votes = E) %>%
-  mutate(percent = str_trim(percent),
-         percent = str_remove(percent, "\\.%"),  # 3 tweets have .%--a typo
-         percent = str_remove(percent, "%"),
-         percent = as.numeric(percent),
-         share = percent/100) %>%
-  select(-percent) %>%
+  select(text, time, state, candidate = A, party = B, votes = E) %>%
   mutate(votes = str_remove(votes, " votes"),
          votes = str_remove_all(votes, ","),
          votes = as.numeric(votes)) %>%
   mutate(candidate = str_trim(candidate)) %>%
   mutate(time = lubridate::ymd_hms(time)) %>%
-  select(state, time, candidate, votes, share, text) %>%
+  select(state, time, candidate, votes, text) %>%
   glimpse()
 
 r %>%
   write_rds("returns.rds") %>%
   write_csv("returns.csv")
-
-ggplot(r, aes(x = time, y = share, color = candidate)) + 
-  geom_line() + 
-  facet_wrap(vars(state), scales = "free") + 
-  scale_y_log10()
